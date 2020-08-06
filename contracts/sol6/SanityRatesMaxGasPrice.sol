d68c72090d9f6b1a24e560f15141b17efb624936
Anton Buenavista <ay.buenavista@gmail.com>
2020-08-06 11:58:49 +0800

    Fixes based on CR

--- contracts/sol6/SanityRatesMaxGasPrice.sol
+++ contracts/sol6/SanityRatesMaxGasPrice.sol
@@ -1,50 +1,48 @@
 pragma solidity 0.6.6;
 
 import "./utils/Utils5.sol";
-import "./utils/WithdrawableNoModifiers.sol";
+import "./utils/Withdrawable3.sol";
 
 
-contract SanityRatesMaxGasPrice is WithdrawableNoModifiers, Utils5 {
+contract SanityRatesMaxGasPrice is Withdrawable3, Utils5 {
     mapping(address => uint256) public tokenRate;
     mapping(address => uint256) public reasonableDiffInBps;
-    uint256 internal maxGasPriceValue = 50 wei * 1000000000; // 50 gwei
+    uint256 public maxGasPriceWei = 50 * 1000 * 1000 * 1000; // 50 gwei
 
     event SanityMaxGasPriceSet(uint256 maxGasPrice);
 
-    constructor(address _admin) public WithdrawableNoModifiers(_admin) {
-        require(_admin != address(0));
+    constructor(address _admin) public Withdrawable3(_admin) {
+        require(_admin != address(0), "admin 0");
         admin = _admin;
     }
 
-    function setReasonableDiff(IERC20[] memory srcs, uint256[] memory diff) public {
-        onlyAdmin();
-        require(srcs.length == diff.length);
+    function setReasonableDiff(IERC20[] memory srcs, uint256[] memory diff) public onlyAdmin {
+        require(srcs.length == diff.length, "srcs,diff length mismatch");
         for (uint256 i = 0; i < srcs.length; i++) {
-            require(diff[i] <= 100 * 100);
+            require(diff[i] <= 100 * 100, "Diff > 10000 BPS");
             reasonableDiffInBps[address(srcs[i])] = diff[i];
         }
     }
 
-    function setMaxGasPrice(uint256 _maxGasPrice) public {
-        onlyOperator();
-        require(_maxGasPrice > 0);
-        maxGasPriceValue = _maxGasPrice;
-        emit SanityMaxGasPriceSet(maxGasPriceValue);
+    function setMaxGasPriceWei(uint256 _maxGasPriceWei) public onlyOperator {
+        require(_maxGasPriceWei > 0, "maxGasPriceWei must be > 0");
+        maxGasPriceWei = _maxGasPriceWei;
+        emit SanityMaxGasPriceSet(maxGasPriceWei);
     }
 
-    function setSanityRates(IERC20[] memory srcs, uint256[] memory rates) public {
-        onlyOperator();
-        require(srcs.length == rates.length);
+    /// @dev You can disable a token by setting the token's sanity rate to MAX_RATE
+    function setSanityRates(IERC20[] memory srcs, uint256[] memory rates) public onlyOperator {
+        require(srcs.length == rates.length, "srcs,rates length mismatch");
 
         for (uint256 i = 0; i < srcs.length; i++) {
-            require(rates[i] <= MAX_RATE);
+            require(rates[i] <= MAX_RATE, "rate > maxRate (10**25)");
             tokenRate[address(srcs[i])] = rates[i];
         }
     }
 
     function getSanityRate(IERC20 src, IERC20 dest) public view returns (uint256) {
         if (src != ETH_TOKEN_ADDRESS && dest != ETH_TOKEN_ADDRESS) return 0;
-        if (tx.gasprice > maxGasPriceValue) return 0;
+        if (tx.gasprice > maxGasPriceWei) return 0;
 
         uint256 rate;
         address token;
--- test/sol6/sanityRatesMaxGasPrice.js
+++ test/sol6/sanityRatesMaxGasPrice.js
@@ -1,10 +1,12 @@
 const SanityRates = artifacts.require('SanityRatesMaxGasPrice.sol');
+const MockReserveSanity = artifacts.require("MockReserveSanity.sol");
 const TestToken = artifacts.require('Token.sol');
 const {expectEvent, expectRevert} = require('@openzeppelin/test-helpers');
 const Helper = require('../helper.js');
 const {BPS, precisionUnits, ethAddress, zeroAddress} = require('../helper.js');
 const {web3} = require('@openzeppelin/test-helpers/src/setup');
 const BN = web3.utils.BN;
+const MAX_RATE = new BN(10).pow(new BN(25)); // 10**25
 
 let sanityRates;
 let admin;
@@ -14,10 +16,12 @@ let token;
 let tokens = [];
 let tokenDecimals = [];
 let rates = [];
+let buyRate = []
+let sellRate = [];
 let reasonableDiffs = [];
 
 contract('SanityRatesMaxGasPrice', function (accounts) {
-  it('should init globals and init sanity rates.', async function () {
+  before('init globals and init sanity rates.', async function () {
     admin = accounts[0];
     operator = accounts[5];
 
@@ -37,152 +41,209 @@ contract('SanityRatesMaxGasPrice', function (accounts) {
     await sanityRates.setReasonableDiff(tokens, reasonableDiffs);
   });
 
-  it('check rates for token 0 (where diff is 0) so only tests rates.', async function () {
-    const tokenToEthRate = await sanityRates.getSanityRate(tokens[0], ethAddress);
-    Helper.assertEqual(tokenToEthRate, rates[0], 'unexpected rate');
-
-    const expectedEthToToken = precisionUnits.mul(precisionUnits).div(tokenToEthRate);
-    const ethToTokenRate = await sanityRates.getSanityRate(ethAddress, tokens[0]);
-    Helper.assertEqual(expectedEthToToken, ethToTokenRate, 'unexpected rate');
-  });
-
-  it('check rates with reasonable diff.', async function () {
-    const tokenInd = 1;
-    const expectedTokenToEthRate = rates[tokenInd]
-      .mul(BPS.add(reasonableDiffs[tokenInd]))
-      .div(BPS);
-
-    const tokenToEthRate = await sanityRates.getSanityRate(tokens[tokenInd], ethAddress);
-    Helper.assertEqual(tokenToEthRate, expectedTokenToEthRate, 'unexpected rate');
-
-    const expectedEthToToken = precisionUnits
-      .mul(precisionUnits)
-      .div(rates[tokenInd])
-      .mul(BPS.add(reasonableDiffs[tokenInd]))
-      .div(BPS);
-    const ethToTokenRate = await sanityRates.getSanityRate(ethAddress, tokens[tokenInd]);
-    Helper.assertEqual(expectedEthToToken, ethToTokenRate, 'unexpected rate');
-  });
-
-  it("should test can't init this contract with empty contracts (address 0).", async function () {
-    let sanityRatess;
-
-    try {
-      sanityRatess = await SanityRates.new(zeroAddress);
-      assert(false, 'throw was expected in line above.');
-    } catch (e) {
-      assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
-    }
-
-    sanityRatess = await SanityRates.new(admin);
-  });
-
-  it("should test can't init diffs when array lengths aren't the same.", async function () {
-    reasonableDiffs.push(8);
-    try {
+  describe("test sanity rates with maxGasPrice setting", function() {
+    it('check rates for token 0 (where diff is 0) so only tests rates.', async function () {
+      const tokenToEthRate = await sanityRates.getSanityRate(tokens[0], ethAddress);
+      Helper.assertEqual(tokenToEthRate, rates[0], 'unexpected rate');
+  
+      const expectedEthToToken = precisionUnits.mul(precisionUnits).div(tokenToEthRate);
+      const ethToTokenRate = await sanityRates.getSanityRate(ethAddress, tokens[0]);
+      Helper.assertEqual(expectedEthToToken, ethToTokenRate, 'unexpected rate');
+    });
+  
+    it('check rates with reasonable diff.', async function () {
+      const tokenInd = 1;
+      const expectedTokenToEthRate = rates[tokenInd]
+        .mul(BPS.add(reasonableDiffs[tokenInd]))
+        .div(BPS);
+  
+      const tokenToEthRate = await sanityRates.getSanityRate(tokens[tokenInd], ethAddress);
+      Helper.assertEqual(tokenToEthRate, expectedTokenToEthRate, 'unexpected rate');
+  
+      const expectedEthToToken = precisionUnits
+        .mul(precisionUnits)
+        .div(rates[tokenInd])
+        .mul(BPS.add(reasonableDiffs[tokenInd]))
+        .div(BPS);
+      const ethToTokenRate = await sanityRates.getSanityRate(ethAddress, tokens[tokenInd]);
+      Helper.assertEqual(expectedEthToToken, ethToTokenRate, 'unexpected rate');
+    });
+  
+    it("should test can't init this contract with empty contracts (address 0).", async function () {
+      let sanityRatess;
+  
+      try {
+        sanityRatess = await SanityRates.new(zeroAddress);
+        assert(false, 'throw was expected in line above.');
+      } catch (e) {
+        assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
+      }
+  
+      sanityRatess = await SanityRates.new(admin);
+    });
+  
+    it("should test can't init diffs when array lengths aren't the same.", async function () {
+      reasonableDiffs.push(8);
+  
+      await expectRevert(
+        sanityRates.setReasonableDiff(tokens, reasonableDiffs),
+        'srcs,diff length mismatch'
+      );
+  
+      reasonableDiffs.length = tokens.length;
       await sanityRates.setReasonableDiff(tokens, reasonableDiffs);
-      assert(false, 'throw was expected in line above.');
-    } catch (e) {
-      assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
-    }
-
-    reasonableDiffs.length = tokens.length;
-
-    await sanityRates.setReasonableDiff(tokens, reasonableDiffs);
-  });
-
-  it("should test can't init diffs when value > max diff (10000 = 100%).", async function () {
-    reasonableDiffs[0] = new BN(10001);
-
-    try {
+    });
+  
+    it("should test can't init diffs when value > max diff (10000 = 100%).", async function () {
+      reasonableDiffs[0] = new BN(10001);
+  
+      await expectRevert(
+        sanityRates.setReasonableDiff(tokens, reasonableDiffs),
+        'Diff > 10000 BPS'
+      );
+  
+      reasonableDiffs[0] = new BN(10000);
       await sanityRates.setReasonableDiff(tokens, reasonableDiffs);
-      assert(false, 'throw was expected in line above.');
-    } catch (e) {
-      assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
-    }
-
-    reasonableDiffs[0] = new BN(10000);
-
-    await sanityRates.setReasonableDiff(tokens, reasonableDiffs);
-  });
-
-  it("should test can't init rates when array lengths aren't the same.", async function () {
-    rates.push(new BN(8));
-    try {
+    });
+  
+    it("should test can't init rates when array lengths aren't the same.", async function () {
+      rates.push(new BN(8));
+  
+      await expectRevert(
+        sanityRates.setSanityRates(tokens, rates, {from: operator}),
+        'srcs,rates length mismatch'
+      );
+  
+      rates.length = tokens.length;
       await sanityRates.setSanityRates(tokens, rates, {from: operator});
-      assert(false, 'throw was expected in line above.');
-    } catch (e) {
-      assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
-    }
-
-    rates.length = tokens.length;
-
-    await sanityRates.setSanityRates(tokens, rates, {from: operator});
-  });
-
-  it('should test reverts when sanity rate > maxRate (10**25).', async function () {
-    const legalRate = new BN(10).pow(new BN(25));
-    const illegalRate = legalRate.add(new BN(1));
-
-    rates[0] = illegalRate;
-
-    try {
+    });
+  
+    it('should test reverts when sanity rate > maxRate (10**25).', async function () {
+      const legalRate = MAX_RATE;
+      const illegalRate = MAX_RATE.add(new BN(1));
+  
+      rates[0] = illegalRate;
+  
+      await expectRevert(
+        sanityRates.setSanityRates(tokens, rates, {from: operator}),
+        'rate > maxRate (10**25)'
+      );
+  
+      rates[0] = legalRate;
       await sanityRates.setSanityRates(tokens, rates, {from: operator});
-      assert(false, 'throw was expected in line above.');
-    } catch (e) {
-      assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
-    }
-
-    rates[0] = legalRate;
-    await sanityRates.setSanityRates(tokens, rates, {from: operator});
-  });
-
-  it('should test return rate 0 when both are tokens (no ether).', async function () {
-    let rate0 = await sanityRates.getSanityRate(tokens[1], tokens[2]);
-    Helper.assertEqual(rate0, 0, '0 rate expected');
-
-    rate0 = await sanityRates.getSanityRate(tokens[0], tokens[1]);
-    Helper.assertEqual(rate0, 0, '0 rate expected');
-
-    rate0 = await sanityRates.getSanityRate(tokens[2], tokens[3]);
-    Helper.assertEqual(rate0, 0, '0 rate expected');
-  });
-
-  it('should test setting max gas price.', async function () {
-    const gasPrice = web3.utils.toWei('100', 'gwei');
-    const txResult = await sanityRates.setMaxGasPrice(gasPrice, {from: operator});
-
-    expectEvent(txResult, 'SanityMaxGasPriceSet', {
-      maxGasPrice: gasPrice
+    });
+  
+    it('should test return rate 0 when both are tokens (no ether).', async function () {
+      let rate0 = await sanityRates.getSanityRate(tokens[1], tokens[2]);
+      Helper.assertEqual(rate0, 0, '0 rate expected');
+  
+      rate0 = await sanityRates.getSanityRate(tokens[0], tokens[1]);
+      Helper.assertEqual(rate0, 0, '0 rate expected');
+  
+      rate0 = await sanityRates.getSanityRate(tokens[2], tokens[3]);
+      Helper.assertEqual(rate0, 0, '0 rate expected');
+    });
+  
+    it('should test setting max gas price.', async function () {
+      const gasPrice = web3.utils.toWei('100', 'gwei');
+      const txResult = await sanityRates.setMaxGasPriceWei(gasPrice, {from: operator});
+  
+      expectEvent(txResult, 'SanityMaxGasPriceSet', {
+        maxGasPrice: gasPrice
+      });
+    });
+  
+    it('should test reverts when not operator is setting max gas price.', async function () {
+      const gasPrice = web3.utils.toWei('100', 'gwei');
+  
+      await expectRevert(
+        sanityRates.setMaxGasPriceWei(gasPrice),
+        "only operator"
+      );
+    });
+  
+    it('should test reverts setting max gas price to 0.', async function () {
+      const gasPrice = web3.utils.toWei('0', 'gwei');
+  
+      await expectRevert(
+        sanityRates.setMaxGasPriceWei(gasPrice, {from: operator}),
+        "maxGasPriceWei must be > 0"
+      );
+    });
+  
+    it('should test sanity rate is 0 when tx gas price > maxGasPrice.', async function () {
+      const tokenToEthRate = await sanityRates.getSanityRate(tokens[0], ethAddress, {
+        gasPrice: web3.utils.toWei('101', 'gwei'),
+      });
+      Helper.assertEqual(tokenToEthRate, 0, 'unexpected rate');
     });
   });
 
-  it('should test reverts when not operator is setting max gas price.', async function () {
-    const gasPrice = web3.utils.toWei('100', 'gwei');
+  describe("test reserve that uses SanityRatesMaxGasPrice", function() {
+    before('setup mock reserve with sanity.', async function () {
+      mockReserve = await MockReserveSanity.new();
+      await mockReserve.setContracts(sanityRates.address);
 
-    try {
-      await sanityRates.setMaxGasPrice(gasPrice);
-      assert(false, 'throw was expected in line above.');
-    } catch (e) {
-      assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
-    }
-  });
-
-  it('should test reverts setting max gas price to 0.', async function () {
-    const gasPrice = web3.utils.toWei('0', 'gwei');
-
-    try {
-      await sanityRates.setMaxGasPrice(gasPrice, {from: operator});
-      assert(false, 'throw was expected in line above.');
-    } catch (e) {
-      assert(Helper.isRevertErrorMessage(e), 'expected throw but got: ' + e);
-    }
-  });
-
-  it('should test sanity rate is 0 when tx gas price > maxGasPrice.', async function () {
-    const tokenToEthRate = await sanityRates.getSanityRate(tokens[0], ethAddress, {
-      gasPrice: web3.utils.toWei('101', 'gwei'),
+      for (let i = 0; i < numTokens; i++) {
+        buyRate[i] = precisionUnits.mul(precisionUnits).div(rates[i]);
+        sellRate[i] = rates[i];
+        await mockReserve.setRate(tokens[i], buyRate[i], sellRate[i]);
+      }
     });
-    Helper.assertEqual(tokenToEthRate, 0, 'unexpected rate');
+
+    it('should test getting conversion rate.', async function () {
+      const amountWei = new BN(2);
+      const currentBlock = await Helper.getCurrentBlock();
+
+      let conversionRate;
+      for (let i = 0; i < numTokens; i++) {
+        conversionRate = await mockReserve.getConversionRate(ethAddress, tokens[i], amountWei, currentBlock);
+        Helper.assertEqual(conversionRate, buyRate[i], 'unexpected rate');
+        
+        conversionRate = await mockReserve.getConversionRate(tokens[i], ethAddress, amountWei, currentBlock);
+        Helper.assertEqual(conversionRate, sellRate[i], 'unexpected rate');
+      }
+    });
+
+    it('should test getting 0 conversion rate when tx gas price > maxGasPrice.', async function () {
+      const amountWei = new BN(2);
+      const currentBlock = await Helper.getCurrentBlock();
+
+      let conversionRate;
+      for (let i = 0; i < numTokens; i++) {
+        conversionRate = await mockReserve.getConversionRate(ethAddress, tokens[i], amountWei, currentBlock, {
+          gasPrice: web3.utils.toWei('101', 'gwei'),
+        });
+        Helper.assertEqual(conversionRate, 0, 'unexpected rate');
+        
+        conversionRate = await mockReserve.getConversionRate(tokens[i], ethAddress, amountWei, currentBlock, {
+          gasPrice: web3.utils.toWei('101', 'gwei'),
+        });
+        Helper.assertEqual(conversionRate, 0, 'unexpected rate');
+      }
+    });
+
+  //   it('should test getting 0 conversion rate when token sanity rate is set to MAX_RATE.', async function () {
+  //     let maxRates = []
+  //     for (let i = 0; i < numTokens; i++) {
+  //       maxRates[i] = MAX_RATE;
+  //     }
+  
+  //     await sanityRates.setSanityRates(tokens, maxRates, {from: operator});
+
+  //     const amountWei = new BN(2);
+  //     const currentBlock = await Helper.getCurrentBlock();
+
+  //     let conversionRate;
+  //     for (let i = 0; i < numTokens; i++) {
+  //       conversionRate = await mockReserve.getConversionRate(ethAddress, tokens[i], amountWei, currentBlock);
+  //       console.log(`buy rate: ${conversionRate}`)
+  //       Helper.assertEqual(conversionRate, 0, 'unexpected rate');
+        
+  //       conversionRate = await mockReserve.getConversionRate(tokens[i], ethAddress, amountWei, currentBlock);
+  //       console.log(`sell rate: ${conversionRate}`)
+  //       Helper.assertEqual(conversionRate, 0, 'unexpected rate');
+  //     }
+  //   });
   });
 });
